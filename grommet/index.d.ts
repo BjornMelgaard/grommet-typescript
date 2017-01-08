7c3cce511e96ad2a1be41a5376514767e5d63ba9
Ryan Collins <rccproductions@mac.com>
2017-01-08 16:12:52 -0500

    Fix: styling for copied interface and classes

--- grommet/index.d.ts
+++ grommet/index.d.ts
@@ -94,7 +94,7 @@ declare namespace Grommet {
     render(): JSX.Element
   }
 
-  interface BoxProps<T> {
+  export interface BoxProps<T> {
     a11yTitle?: string | T;
     announce?: boolean;
     align?: 'start' | 'center' | 'end' | 'baseline' | 'stretch';
@@ -144,620 +144,480 @@ declare namespace Grommet {
     texture?: React.ReactNode | string;
     wrap?: boolean;
   }
-  export class Box extends React.Component<BoxProps, any>{
+  export class Box extends React.Component<BoxProps<any>, any>{
     render(): JSX.Element
   }
 
   interface ButtonProps {
-
+    a11yTitle?: string;
+    accent?: boolean;
+    align?: 'start' | 'center' | 'end';
+    fill?: boolean;
+    href?: string;
+    icon?: React.ReactElement<any>;
+    label?: React.ReactNode;
+    method?: 'push' | 'replace';
+    onClick?: Function;
+    path?: string;
+    plain?: boolean;
+    primary?: boolean;
+    reverse?: boolean;
+    secondary?: boolean;
+    type?: 'button' | 'reset' | 'submit';
   }
-
   export class Button extends React.Component<ButtonProps, any>{
-
+    render(): JSX.Element
   }
 
-  interface CardProps {
-
+  interface CardProps extends BoxProps<any> {
+    contentPad?: GrommetBoxTypes.PadSizes | {
+      between?: GrommetBoxTypes.PadSizes;
+      horizontal?: GrommetBoxTypes.PadSizes;
+      vertical?: GrommetBoxTypes.PadSizes;
+    };
+    description?: string | React.ReactElement<any>;
+    heading?: string | React.ReactElement<any>;
+    headingStrong?: boolean;
+    label?: string | React.ReactElement<any>;
+    link?: React.ReactElement<any>;
+    textSize?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
+    thumbnail?: string | React.ReactElement<any>;
+    truncate?: boolean;
+    video?: { source: string, type?: string } | React.ReactElement<any>;
   }
-
   export class Card extends React.Component<CardProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface CarouselProps {
-
+    a11yTitle?: string;
+    autoplay?: boolean;
+    autoplaySpeed?: number;
+    infinite?: boolean;
+    persistentNav?: boolean;
   }
-
   export class Carousel extends React.Component<CarouselProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface chartProps {
 
   }
-
   export class chart extends React.Component<chartProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface CheckBoxProps {
 
   }
-
   export class CheckBox extends React.Component<CheckBoxProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface ColumnsProps {
 
   }
 
   export class Columns extends React.Component<ColumnsProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface DateTimeProps {
 
   }
-
   export class DateTime extends React.Component<DateTimeProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface DistributionProps {
 
   }
-
   export class Distribution extends React.Component<DistributionProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface FooterProps {
 
   }
-
   export class Footer extends React.Component<FooterProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface FormProps {
 
   }
-
   export class Form extends React.Component<FormProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface FormattedMessageProps {
 
   }
-
   export class FormattedMessage extends React.Component<FormattedMessageProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface FormFieldProps {
 
   }
-
   export class FormField extends React.Component<FormFieldProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface FormFieldsProps {
 
   }
-
   export class FormFields extends React.Component<FormFieldsProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface GrommetProps {
 
   }
-
   export class Grommet extends React.Component<GrommetProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface HeaderProps {
 
   }
-
   export class Header extends React.Component<HeaderProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface HeadingProps {
 
   }
-
   export class Heading extends React.Component<HeadingProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface HeadlineProps {
 
   }
-
   export class Headline extends React.Component<HeadlineProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface HeroProps {
 
   }
-
   export class Hero extends React.Component<HeroProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface ImageProps {
 
   }
-
   export class Image extends React.Component<ImageProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface LabelProps {
 
   }
-
   export class Label extends React.Component<LabelProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface LayerProps {
 
   }
-
   export class Layer extends React.Component<LayerProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface LegendProps {
 
   }
-
   export class Legend extends React.Component<LegendProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface ListProps {
 
   }
-
   export class List extends React.Component<ListProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface ListItemProps {
 
   }
-
   export class ListItem extends React.Component<ListItemProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface LoginFormProps {
 
   }
-
   export class LoginForm extends React.Component<LoginFormProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface MapProps {
 
   }
-
   export class Map extends React.Component<MapProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface MarkdownProps {
 
   }
-
   export class Markdown extends React.Component<MarkdownProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface MenuProps {
 
   }
-
   export class Menu extends React.Component<MenuProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface MeterProps {
 
   }
-
   export class Meter extends React.Component<MeterProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface NotificationProps {
 
   }
-
   export class Notification extends React.Component<NotificationProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface NumberInputProps {
-
+    render(): JSX.Element
   }
-
   export class NumberInput extends React.Component<NumberInputProps, any>{
 
   }
 
-
-
-
   interface ObjectProps {
 
   }
-
   export class Object extends React.Component<ObjectProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface ParagraphProps {
 
   }
-
   export class Paragraph extends React.Component<ParagraphProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface QuoteProps {
 
   }
-
   export class Quote extends React.Component<QuoteProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface RadioButtonProps {
 
   }
-
   export class RadioButton extends React.Component<RadioButtonProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface SearchProps {
 
   }
-
   export class Search extends React.Component<SearchProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface SearchInputProps {
 
   }
-
   export class SearchInput extends React.Component<SearchInputProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface SectionProps {
 
   }
-
   export class Section extends React.Component<SectionProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface SelectProps {
 
   }
-
   export class Select extends React.Component<SelectProps, any>{
-
+    render(): JSX.Element
   }
-
-
-
 
   interface SidebarProps {
 
   }
-
   export class Sidebar extends React.Component<SidebarProps, any>{
-
+    render(): JSX.Element
   }
 
   interface SkipLinkAnchorProps {
 
   }
-
   export class SkipLinkAnchor extends React.Component<SkipLinkAnchorProps, any>{
-
+    render(): JSX.Element
   }
 
   interface SkipLinksProps {
 
   }
-
   export class SkipLinks extends React.Component<SkipLinksProps, any>{
-
+    render(): JSX.Element
   }
 
   interface SocialShareProps {
 
   }
-
   export class SocialShare extends React.Component<SocialShareProps, any>{
-
+    render(): JSX.Element
   }
 
   interface SplitProps {
 
   }
   export class Split extends React.Component<SplitProps, any>{
-
+    render(): JSX.Element
   }
 
   interface SunBurstProps {
 
   }
   export class SunBurst extends React.Component<SunBurstProps, any>{
-
+    render(): JSX.Element
   }
 
   interface SVGIconProps {
 
   }
   export class SVGIcon extends React.Component<SVGIconProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TabProps {
 
   }
   export class Tab extends React.Component<TabProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TableProps {
 
   }
   export class Table extends React.Component<TableProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TableHeaderProps {
 
   }
-
   export class TableHeader extends React.Component<TableHeaderProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TableRowProps {
 
   }
-
   export class TableRow extends React.Component<TableRowProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TabsProps {
 
   }
-
   export class Tabs extends React.Component<TabsProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TBDProps {
 
   }
-
   export class TBD extends React.Component<TBDProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TextInputProps {
 
   }
-
   export class TextInput extends React.Component<TextInputProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TileProps {
 
   }
-
   export class Tile extends React.Component<TileProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TilesProps {
 
   }
-
   export class Tiles extends React.Component<TilesProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TimestampProps {
 
   }
-
   export class Timestamp extends React.Component<TimestampProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TipProps {
 
   }
-
   export class Tip extends React.Component<TipProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TitleProps {
 
   }
-
   export class Title extends React.Component<TitleProps, any>{
-
+    render(): JSX.Element
   }
 
   interface ToastProps {
 
   }
-
   export class Toast extends React.Component<ToastProps, any>{
-
+    render(): JSX.Element
   }
 
   interface TopologyProps {
 
   }
-
   export class Topology extends React.Component<TopologyProps, any>{
-
+    render(): JSX.Element
   }
 
   interface ValueProps {
 
   }
-
   export class Value extends React.Component<ValueProps, any>{
-
+    render(): JSX.Element
   }
 
   interface VideoProps {
 
   }
-
   export class Video extends React.Component<VideoProps, any>{
-
+    render(): JSX.Element
   }
 
   interface WorldMapProps {
 
   }
-
   export class WorldMap extends React.Component<WorldMapProps, any>{
-
+    render(): JSX.Element
   }
 }
